a43f7e4761b26d51c1f6619a6324ab35c8961845
Roman <rozario91208@gmail.com>
2020-03-24 22:19:57 +0200

    create select for right answer

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,6 +3,7 @@ import classes from './QuizCreator.scss';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import { createControl } from '../../form/formFramework';
+import Select from '../../components/UI/Select/Select';
 
 function createOptionControl(number) {
   return createControl({
@@ -30,6 +31,7 @@ export default class QuizCreator extends Component {
 
   state = {
     quiz: [],
+    rightAnswerId: 1,
     formControls: createFormControls()
   }
 
@@ -67,7 +69,25 @@ export default class QuizCreator extends Component {
     })
   }
 
+  selectChangeHandler = e => {
+    this.setState({
+      rightAnswerId: +e.target.value
+    })
+  }
+
   render() {
+    const select = <Select
+      label={'Выберте правильный ответ'}
+      value={this.state.rightAnswerId}
+      onChange={this.selectChangeHandler}
+      options={[
+        { text: 1, value: 1 },
+        { text: 2, value: 2 },
+        { text: 3, value: 3 },
+        { text: 4, value: 4 }
+      ]}
+    />
+
     return (
       <div className={classes.QuizCreator}>
         <div>
@@ -78,7 +98,7 @@ export default class QuizCreator extends Component {
             {this.renderControls()}
 
 
-            <select></select>
+            {select}
 
             <Button
               type="primary"
